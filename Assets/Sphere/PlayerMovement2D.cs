61c8c556dbbd5e7845a0ebdf4316f9370750200a
mgart_000 <mgartz@gmail.com>
2016-01-17 17:03:29 -0600

    Fix glitch where sphere stopped moving when colliding with walls sometimes.

--- Assets/Sphere/PlayerMovement2D.cs
+++ Assets/Sphere/PlayerMovement2D.cs
@@ -6,7 +6,7 @@ public class PlayerMovement2D : MonoBehaviour {
 	float speed = 2f;
 
 	// Update is called once per frame
-	void Update() {
+	void FixedUpdate() {
 		float forwardSpeed = Input.GetAxis ("Vertical");
 		float lateralSpeed = Input.GetAxis ("Horizontal");
 		if (Mathf.Abs (forwardSpeed) + Mathf.Abs (lateralSpeed) > 1) {
@@ -22,7 +22,7 @@ public class PlayerMovement2D : MonoBehaviour {
 		Vector3 lateral = lateralSpeed * new Vector3 (Mathf.Cos(angles.y), 0, -Mathf.Sin(angles.y));
 		Vector3 directForce = (forward + lateral);
 
-		if (directForce.magnitude < 0.01f || Vector3.Dot (rigidbody.velocity, directForce) < 0) {
+		if (directForce.magnitude < 0.01f || Vector3.Dot (rigidbody.velocity, directForce) < -0.1f) {
 			rigidbody.velocity = new Vector3 (0, rigidbody.velocity.y, 0);
 		}
 		else {
